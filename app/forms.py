2ce2860ee44a80032af35cc5a380549fbaa0c57d
jibieta <joseibieta@gmail.com>
2019-09-27 17:31:38 -0300

    ok

--- app/forms.py
+++ app/forms.py
@@ -8,13 +8,22 @@ class ResultForm(ModelForm):
 
     class Meta:
         model = Result
+        labels = {
+            'mtype': 'Tipo de pelea',
+            'challenging': 'Retador',
+            'rival': 'Rival',
+            'challenging_score': 'Marcador retador',
+            'rival_score': 'Marcador rival',
+            'tournament': 'Seleccionar torneo',
+            'replay_url': 'URL Replay'
+        }        
         fields = ['mtype', 'challenging', 'rival', 'challenging_score', 'rival_score', 'tournament', 'replay_url']
         widgets = {
             'mtype': forms.Select(attrs={'class': 'form-control'}),
             'challenging': forms.Select(attrs={'class': 'form-control'}),
             'rival': forms.Select(attrs={'class': 'form-control'}),
-            'challenging_score': forms.TextInput(attrs={'id': 'id_title', 'class': 'form-control'}),
-            'rival_score': forms.TextInput(attrs={'id': 'id_title', 'class': 'form-control'}),
+            'challenging_score': forms.NumberInput(attrs={'id': 'id_title', 'class': 'form-control'}),
+            'rival_score': forms.NumberInput(attrs={'id': 'id_title', 'class': 'form-control'}),
             'tournament': forms.Select(attrs={'class': 'form-control'}),
             'replay_url': forms.TextInput(attrs={'class': 'form-control'}),
         }
